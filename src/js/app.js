afb7e2f93a68306912303a652afd5ed9865ded47
tanyavulcheva23 <tantellka07@gmail.com>
2022-12-01 16:33:54 +0200

    task

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,11 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
+  const image = document.querySelector(".image")
+  image.addEventListener("click", () => {
+      image.style.transform = "scale(2)"
+  })
+
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
